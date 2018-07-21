e1b9e4a75a3fee31bf5861822844c636666c869b
Masahiro Yoshimoto <yoshimoto@flab.phys.nagoya-u.ac.jp>
2018-07-22 01:50:50 +0900

    fix grammer mistake about rotate function

--- affine6p/transform.py
+++ affine6p/transform.py
@@ -38,21 +38,21 @@ class Transform(object):
         # else
         return list(map(transform_one, points))
 
-    def rotation(self, points):
+    def rotate(self, points):
         '''
         Parameter
             p
                 point [x, y] or list of points [[x1,y1], [x2,y2], ...]
         '''
-        def rotation_one(point):
+        def rotate_one(point):
             return [self.params[0] * point[0] + self.params[1] * point[1],
                     self.params[2] * point[0] + self.params[3] * point[1]]
 
         if not isinstance(points[0], list):
             # Single point
-            return rotation_one(points)
+            return rotate_one(points)
         # else
-        return list(map(rotation_one, points))
+        return list(map(rotate_one, points))
 
     def transform_inv(self, points):
         '''
@@ -72,13 +72,13 @@ class Transform(object):
         # else
         return list(map(transform_inv_one, points))
 
-    def rotation_inv(self, points):
+    def rotate_inv(self, points):
         '''
         Parameter
             p
                 point [x, y] or list of points [[x1,y1], [x2,y2], ...]
         '''
-        def rotation_inv_one(point):
+        def rotate_inv_one(point):
             det = self.params[0] * self.params[3] - \
                 self.params[2] * self.params[1]
             return [(self.params[3] * point[0] - self.params[1] * point[1]) / det,
@@ -86,9 +86,9 @@ class Transform(object):
 
         if not isinstance(points[0], list):
             # Single point
-            return rotation_inv_one(points)
+            return rotate_inv_one(points)
         # else
-        return list(map(rotation_inv_one, points))
+        return list(map(rotate_inv_one, points))
 
     def get_matrix(self):
         return [[self.params[0], self.params[1], self.params[4]],
